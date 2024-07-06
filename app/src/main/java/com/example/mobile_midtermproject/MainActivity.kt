1db2ac29c46b99c15e18217752f664ec8ede9c08
BachNgoH <nlmbao2015@gmail.com>
2024-07-06 13:22:38 +0700

    feature (font): add custom fonts

--- app/src/main/java/com/example/mobile_midtermproject/MainActivity.kt
+++ app/src/main/java/com/example/mobile_midtermproject/MainActivity.kt
@@ -7,13 +7,50 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Step 1: Define your custom font family
+val customFontFamily = FontFamily(
+    Font(R.font.poppins_black, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
+// Step 2: Create a custom Typography
+val customTypography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = customFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = customFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = customFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = customFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp
+    ),
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
+            MaterialTheme (typography = customTypography) {
                 Surface(color = MaterialTheme.colorScheme.background) {
                     MainNavigation()
                 }
--- app/src/main/java/com/example/mobile_midtermproject/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mobile_midtermproject/ui/screens/HomeScreen.kt
@@ -6,8 +6,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
+import com.example.mobile_midtermproject.R
 
 @Composable
 fun HomeScreen() {
